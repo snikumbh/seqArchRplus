4290ccf41eaf49922d3f89bb2f3b19e2986bad50
snikumbh <sarvesh.nikumbh@gmail.com>
2022-05-12 15:58:18 +0100

    fixed curate clusters; added NEWS.md to broadcast changes

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+# seqArchRplus 0.99.0.3
+
+## Bux-fixes
+* Fixed bugs in `curate_clusters()` function, and touch up its documentation
+
+
+# seqArchRplus 0.99.0.2
+
+## New features
+* (User-facing) Parallelization support to speed up annotating genomic regions
--- R/seqArchRplus_curate_clusters.R
+++ R/seqArchRplus_curate_clusters.R
@@ -35,7 +35,6 @@
 #' that needs to be put into a new, independent cluster of itself, use
 #' 0 (as numeric). Both `need_change` and `change_to` should be empty lists if
 #' no re-assignment is to be performed.
-#' If any element is to be put into a new cluster, use a numeral 0 in change_to
 #'
 #' @param final Logical, set to TRUE or FALSE
 #'
@@ -49,23 +48,58 @@
 #' The resulting clustering is visualized by a dendrogram, color-coded cluster
 #' assignments, and corresponding sequence logos. Using this visualization,
 #' the user can identify/estimate the (nearly right) number of clusters to cut
-#' the dendrogram. The first call uses K = 1.
+#' the dendrogram. The first call uses K = 2.
 #'
 #' 2. Re-call the function with the identified value of K. Look at the
-#' visualization to determine if it is good enough, i.e., it requires minor
-#' re-assignments).
+#' visualization to determine if it is good enough, i.e., it requires only
+#' minor re-assignments).
 #'
 #' 3. Identify cases of cluster assignments that you wish to re-assign to
 #' different clusters. These can be noted as a list and supplied in a
 #' subsequent call to the function.
 #'
-#' 2. In the final call to the function, set `final = TRUE`, supply the re-
+#' 4. In the final call to the function, set `final = TRUE`, supply the re-
 #' assignments as two lists `need_change` and `change_to`.
 #'
+#' More on re-assignments using \code{need_change} and \code{change_to}:
+#' If any element is to be put into a new cluster, use a numeral 0 in change_to.
+#' This can be done in either scenario: when any element is re-assigned as a
+#' singleton cluster of itself, or as clustered with some other element coming
+#' from some other existing cluster. Consider, for instance, among some 33
+#' clusters identified by seqArchR, the following re-assignments are
+#' executed.
+#'
+#' Original_clustering <- list(c(1), c(2,3), c(4,5), c(6, 7))
+#' need_change <- list(c(5), c(2), c(3, 6))
+#' change_to <- list(1, 0, 0)
+#'
+#' In the above, element 5 is re-assigned to the cluster containing element 1.
+#' Element 2 is re-assigned to a new, singleton cluster of itself, while
+#' elements 3 and 6 (which initially can belong to same/any two clusters) are
+#' collated together. Note that it is important to combine 21 and 30 using
+#' \code{c()}.
+#'
+#' Also see examples below.
+#'
+#'
 #' @return
+#' This function returns the input sequence clusters as a list and writes a
+#' PDF file to disk showing the dendrogram + sequence logos to help
+#' perform curation and document it.
+#'
+#'
+#' When `final = FALSE`, the clusters ordered according to the hclust
+#' result/dendrogram are returned. The PDF file shows only the dendrogram +
+#' sequence logos of clusters (clusters ordered by \code{hclust} ordering)
+#'
 #' When `final = TRUE`, the return value of the function is the clusters
-#' with re-assignments executed. Otherwise, the clusters are ordered by
-#' according to the hclust result/dendrogram.
+#' with re-assignments executed.
+#' The PDF file showing the dendrogram + sequence logos now has an additional
+#' panel showing the sequence logos upon collation of clusters as specified by
+#' the re-assignments. Also the cluster IDs in the dendrograms have colors
+#' showing the re-assignments, i.e., elements that were re-assigned to different
+#'  clusters, also have appropriate color changes reflected.
+#'
 #'
 #' @importFrom stats cutree median
 #'
@@ -160,11 +194,18 @@ curate_clusters <- function(sname, use_aggl = "ward.D", use_dist = "euclid",
     } else {
         temp_clusts <- stats::cutree(clusts_reord, k = use_cutk)
         names(temp_clusts) <- NULL
+
+        temp_clusts <- .handle_cl_reassignment(
+            need_change = need_change,
+            change_to = change_to,
+            temp_clusts = temp_clusts, ret_cl_assignments = TRUE
+        )
         clust_list <- .handle_cl_reassignment(
             need_change = need_change,
             change_to = change_to,
-            temp_clusts = temp_clusts
+            temp_clusts = temp_clusts, ret_cl_assignments = FALSE
         )
+
 
         seqs_clusters_as_list <- seqArchR::collate_clusters(
             to_clust = clust_list,
@@ -174,6 +215,7 @@ curate_clusters <- function(sname, use_aggl = "ward.D", use_dist = "euclid",
         )
         ##
         use_color <- scales::hue_pal()(length(unique(temp_clusts)))
+
         sam_foo2 <- .plot_dend_arch(
             arch_plot = sam_foo, fname = fname,
             use_ht = 60,
@@ -206,15 +248,18 @@ curate_clusters <- function(sname, use_aggl = "ward.D", use_dist = "euclid",
 ##
 ## need_change is a list of clust IDs that will be reassigned
 ## change_to is a list of clusters they will be assigned to
-## Both the lists have a one-to-one mapping, meaning that element 1 is list
-## need_change is assigned to element 1 in changes_to list
+## Both the lists have a one-to-one mapping, meaning that element 1 in list
+## need_change is assigned element 1 in change_to list
 ##
 ## Use 0 for new, independent clusters
 ## ??Use negative numbers for independent but same clusters for two different
 ## elements
 ##
+## If ret_cl_assignments is TRUE (i.e., return cluster assignments), changed
+## temp_clusts is returned, Otherwise, a clust_list with clusterIDs is returned
+##
 .handle_cl_reassignment <- function(need_change, change_to,
-                                    temp_clusts) {
+                                    temp_clusts, ret_cl_assignments = TRUE) {
     if (!all(lengths(list(need_change, change_to)) == 0)) {
         stopifnot(length(need_change) == length(change_to))
         sec_list_lens <- lengths(change_to)
@@ -224,19 +269,50 @@ curate_clusters <- function(sname, use_aggl = "ward.D", use_dist = "euclid",
         ###
         #
         ## Re-assign here
-        ## Two things can happen:
+        ##
+        ## Following scenarios can arise:
+        ## -- An existing cluster becoming empty/null set due to re-assignments
+        ##      -- members can be re-assigned to another existing cluster
+        ##      -- members can be re-assigned to a new singleton cluster
+        ##         containing only itself (use numeral 0 in change_to)
+        ##      -- members can be re-assigned to a new cluster (non-existent
+        ##         earlier), but this is not a singleton. This can be achieved
+        ##         by using combine in the need_change list entry.
+        ##
+        ## -- Normal re-assignments: those that do not lead to empty clusters
+        ##
+        ##
+        ## Two things can happen when new clusters get created:
         ## A. Some existing cluster can have all its elements re-assigned, then
         ## this cluster is empty
         ## B. Some elements can be re-assigned to completely independent
-        ## clusters, i.e., new clusters. These are marked by numeral 0 in
-        ## change_to. Instead of letting them create confusions, we first
-        ## implement all non-zero re-assignments, and handle resultant
-        ## null sets as below. Only then implement type B re-assignments.
+        ## clusters, i.e., new clusters. These are marked by numeral 0  or
+        ## negative numbers in change_to.
+        ## Instead of letting them create confusions, we first
+        ## implement all non-zero and non-negative  re-assignments, and
+        ## handle resultant null sets as below.
         ##
-        ## Type A re-assignments
-        for (x in seq_along(need_change)) {
-            temp_clusts[need_change[[x]]] <- temp_clusts[change_to[[x]]]
+        ## Finally, implement type B re-assignments.
+        ##
+
+        ##
+        ##
+        ## Re-assignments here change_to is an existing cluster
+
+        pos_idx <- which(unlist(change_to) > 0)
+        if(length(pos_idx) > 0){
+            for (i in seq_along(pos_idx)) {
+                temp_clusts[need_change[[pos_idx[i]]]] <-
+                    temp_clusts[change_to[[pos_idx[i]]]]
+            }
         }
+        # for (x in seq_along(need_change)) {
+        #     if(change_to[[x]] > 0){
+        #         temp_clusts[need_change[[x]]] <- temp_clusts[change_to[[x]]]
+        #     }else{
+        #
+        #     }
+        # }
         ## When reassigning, some clusters may become null sets. They should be
         ## omitted. The below code does that
         existing_clust <- sort(unique(temp_clusts))
@@ -249,17 +325,48 @@ curate_clusters <- function(sname, use_aggl = "ward.D", use_dist = "euclid",
         ## Type B re-assignments
         ##
         zero_idx <- which(unlist(change_to) == 0)
-        ## Make further few clusters
-        nCl <- length(unique(temp_clusts))
-        for (i in seq_along(zero_idx)) {
-            temp_clusts[need_change[[zero_idx[i]]]] <- nCl + i
+        if(length(zero_idx) > 0){
+            ## Make further few clusters
+            nCl <- length(unique(temp_clusts))
+            for (i in seq_along(zero_idx)) {
+                temp_clusts[need_change[[zero_idx[i]]]] <- nCl + i
+            }
+        }
+
+        ## When reassigning, some clusters may become null sets. They should be
+        ## omitted. The below code does that
+        existing_clust <- sort(unique(temp_clusts))
+        for (i in seq_along(existing_clust)) {
+            idx <- which(temp_clusts == existing_clust[i])
+            temp_clusts[idx] <- i
+        }
+
+        neg_idx <- which(unlist(change_to) < 0)
+        if(length(neg_idx) > 0){
+            ## Make further few clusters
+            nCl <- length(unique(temp_clusts))
+            for (i in seq_along(neg_idx)) {
+                temp_clusts[need_change[[neg_idx[i]]]] <- nCl + i
+            }
+        }
+
+        ## When reassigning, some clusters may become null sets. They should be
+        ## omitted. The below code does that
+        existing_clust <- sort(unique(temp_clusts))
+        for (i in seq_along(existing_clust)) {
+            idx <- which(temp_clusts == existing_clust[i])
+            temp_clusts[idx] <- i
         }
     }
-    ## Also need to show alongside, how the final clusters' seqlogos look
-    clust_list <- lapply(unique(temp_clusts), function(x) {
-        which(temp_clusts == x)
-    })
-    clust_list
+    if(!ret_cl_assignments){
+        ## Also need to show alongside, how the final clusters' seqlogos look
+        clust_list <- lapply(unique(temp_clusts), function(x) {
+            which(temp_clusts == x)
+        })
+        return(clust_list)
+    }else{
+        return(temp_clusts)
+    }
 }
 ## =============================================================================
 
